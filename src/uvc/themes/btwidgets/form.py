01c59d9eca57337cef6585ef74b083b53fd061d9
goschtl <ck@novareto.de>
2014-09-25 11:15:18 +0200

    add forms stuff

---
+++ src/uvc/themes/btwidgets/form.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2013 NovaReto GmbH
+# cklinger@novareto.de
+
+
+import uvclight
+
+from dolmen.forms.base import interfaces
+from dolmen.forms.base.widgets import ActionWidget
+from uvc.bootstraptheme import IBootstrapThemeRequest
+from cromlech.browser import ITemplate
+from dolmen.forms.base.interfaces import IForm
+from grokcore.component import adapts, adapter, implementer
+
+
+@adapter(IForm, IBootstrapThemeRequest)
+@implementer(ITemplate)
+def bootstrap_form_template(context, request):
+    """default template for the menu"""
+    return uvclight.get_template('form.cpt', __file__)
+
+
+class ActionWidget(ActionWidget):
+    adapts(
+        interfaces.IAction,
+        interfaces.IFieldExtractionValueSetting,
+        IBootstrapThemeRequest)
+
+    def htmlClass(self):
+        return "action btn btn-default"
