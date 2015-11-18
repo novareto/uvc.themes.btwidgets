78a39d9f869fd21762a763f1cf2c90f6668e5627
Souheil CHELFOUH <trollfot@gmail.com>
2015-11-18 15:32:27 +0100

    Corrected collection display widget

--- src/uvc/themes/btwidgets/widgets/collection.py
+++ src/uvc/themes/btwidgets/widgets/collection.py
@@ -52,3 +52,11 @@ class MultiSelectFieldWidget(MultiChoiceFieldWidget):
 class MultiChoiceDisplayFieldWidget(MultiChoiceFieldWidget):
     name('display')
     template = getTemplate('multichoicedisplayfieldwidget.cpt')
+
+    def renderableChoice(self):
+        current = self.inputValue()
+        base_id = self.htmlId()
+        for i, choice in enumerate(self.choices()):
+            if choice.token in current:
+                yield {'title': choice.title,
+                       'id': base_id + '-' + str(i)}
