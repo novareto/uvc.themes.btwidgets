6dfddf8dba0222887d93633b430ae4791a64f7c6
Christian Klinger <ck@novareto.de>
2016-05-25 09:18:10 +0200

    chagnes

--- src/uvc/themes/btwidgets/widgets/date.py
+++ src/uvc/themes/btwidgets/widgets/date.py
@@ -2,13 +2,14 @@
 
 from dolmen.forms.ztk.widgets import date
 from grokcore.component import adapts
-from . import IBootstrapRequest
+from . import IBootstrapRequest, getTemplate
 from zope.interface import Interface
 
 
 class DateFieldWidget(date.DateFieldWidget):
     adapts(date.DateField, Interface, IBootstrapRequest)
     defaultHtmlClass = ['field', 'field-date', 'form-control']
+    template = getTemplate('datewidget.cpt')
 
 
 class DateFieldDisplayWidget(date.DateFieldDisplayWidget):
--- src/uvc/themes/btwidgets/widgets/time.py
+++ src/uvc/themes/btwidgets/widgets/time.py
@@ -2,13 +2,14 @@
 
 from dolmen.forms.ztk.widgets import time
 from grokcore.component import adapts
-from . import IBootstrapRequest
+from . import IBootstrapRequest, getTemplate
 from zope.interface import Interface
 
 
 class TimeFieldWidget(time.TimeFieldWidget):
     adapts(time.TimeField, Interface, IBootstrapRequest)
     defaultHtmlClass = ['field', 'field-time', 'form-control']
+    template = getTemplate('timewidget.cpt')
     
 
 class TimeFieldDisplayWidget(time.TimeFieldDisplayWidget):
