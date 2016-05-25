6e3fb90bb0e859c135edead842d9864f2c9832e2
Christian Klinger <ck@novareto.de>
2016-05-25 09:18:53 +0200

    Back to development: 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join
 from setuptools import setup, find_packages
 
 name = 'uvc.themes.btwidgets'
-version = '0.5'
+version = '0.6.dev0'
 readme = open('README.txt').read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
